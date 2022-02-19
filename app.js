41d8d69cf4a7a0c7b5e0703166aa69bb1cd386af
konstantinreso <88545523+konstantinreso@users.noreply.github.com>
2022-02-19 18:15:58 +0200

    Homework 1
    Commit homework Lesson 1

---
+++ app.js
@@ -0,0 +1,11 @@
+const { info, warn, error } = require("./utils/logger");
+
+console.log("---------------------");
+console.log("     Sample text:    ");
+console.log("---------------------");
+
+info("This is info message");
+debugger;
+warn("This is warning message");
+debugger;
+error("This is error message");
